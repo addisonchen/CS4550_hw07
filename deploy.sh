9f92976479aaf8b6b814b0d359a98d0a9408d1c7
Addison Chen <addisonjchen@gmail.com>
2021-03-01 04:35:51 +0000

    start and dep

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export SECRET_KEY_BASE=r0ZqmXKFwRGpEtBpS5r18OpghlHZrutfzTIiaXOK9GvQPq9K73KkXlT4yVhAOpMP
+export MIX_ENV=prod
+export PORT=5234
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+export DATABASE_URL=postgres://events:fa6Ahx0eeris@localhost:5432/events_dev
+
+
+echo "Building..."
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+#echo "Stopping old copy of app, if any..."
+#_build/prod/rel/practice/bin/practice stop || true
+
+echo "Starting app..."
+
+PROD=t ./start.sh
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TODO: Enable this script by removing the above.
+
+export SECRET_KEY_BASE=r0ZqmXKFwRGpEtBpS5r18OpghlHZrutfzTIiaXOK9GvQPq9K73KkXlT4yVhAOpMP
+export MIX_ENV=prod
+export PORT=5234
+export DATABASE_URL=postgres://events:fa6Ahx0eeris@localhost:5432/events_dev
+
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/events/bin/events stop || true
+
+echo "Starting app..."
+
+export PORT=5234
+_build/prod/rel/events/bin/events start
+
+# TODO: Add a systemd service file
+#       to start your app on system boot.
+
